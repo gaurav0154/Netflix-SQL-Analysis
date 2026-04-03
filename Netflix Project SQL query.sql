e86720229d5aebeaa6764bd4ae74675a064d8679
gaurav0154 <gauravjoshi131999@gmail.com>
2026-04-03 16:51:14 +0530

    Update Netflix Project SQL query.sql

--- Netflix Project SQL query.sql
+++ Netflix Project SQL query.sql
@@ -2,11 +2,13 @@
 -- Solutions of business problems
 
 -- 1. Count the number of Movies vs TV Shows
+'''sql
 SELECT 
 	type,
 	COUNT(*)
 FROM netflix
 GROUP BY 1
+	'''
 
 -- 2. Find the most common rating for movies and TV shows
 WITH RatingCounts AS (
